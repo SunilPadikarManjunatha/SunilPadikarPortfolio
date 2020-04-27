3a2c9fc995c8a591efdfde513ba1dcdec39e2c5c
SunilPadikarM <43018114+SunilPadikarM@users.noreply.github.com>
2020-04-27 18:32:42 -0400

    Updates

---
+++ precache-manifest.697d7e655576956025bdf9a8bfba8dc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba90bf80cfc9e422cbd31a4cdedab97e",
+    "url": "/SunilPadikarPortfolio/index.html"
+  },
+  {
+    "revision": "472928129ccc2ae6d629",
+    "url": "/SunilPadikarPortfolio/static/css/2.dc56c381.chunk.css"
+  },
+  {
+    "revision": "ac144ca54ee1920318b7",
+    "url": "/SunilPadikarPortfolio/static/css/main.642b4771.chunk.css"
+  },
+  {
+    "revision": "472928129ccc2ae6d629",
+    "url": "/SunilPadikarPortfolio/static/js/2.eb32a057.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SunilPadikarPortfolio/static/js/2.eb32a057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac144ca54ee1920318b7",
+    "url": "/SunilPadikarPortfolio/static/js/main.47819819.chunk.js"
+  },
+  {
+    "revision": "d90954cb002f51aa2044",
+    "url": "/SunilPadikarPortfolio/static/js/runtime-main.09824b92.js"
+  }
+]);
