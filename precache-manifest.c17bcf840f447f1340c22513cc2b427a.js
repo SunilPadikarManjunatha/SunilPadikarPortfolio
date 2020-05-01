a5e5394bb19e9aec60e0e0f9f32b88bdd9b75afb
SunilPadikarM <43018114+SunilPadikarM@users.noreply.github.com>
2020-04-30 20:15:56 -0400

    Updates

---
+++ precache-manifest.c17bcf840f447f1340c22513cc2b427a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b146eb06fba14403304c611eedd7f6a5",
+    "url": "/SunilPadikarPortfolio/index.html"
+  },
+  {
+    "revision": "f97da37061f67b8ea588",
+    "url": "/SunilPadikarPortfolio/static/css/2.dc56c381.chunk.css"
+  },
+  {
+    "revision": "7032d2b1c8b13c3b39cf",
+    "url": "/SunilPadikarPortfolio/static/css/main.0b436f98.chunk.css"
+  },
+  {
+    "revision": "f97da37061f67b8ea588",
+    "url": "/SunilPadikarPortfolio/static/js/2.e0916621.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SunilPadikarPortfolio/static/js/2.e0916621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7032d2b1c8b13c3b39cf",
+    "url": "/SunilPadikarPortfolio/static/js/main.346e8c59.chunk.js"
+  },
+  {
+    "revision": "d90954cb002f51aa2044",
+    "url": "/SunilPadikarPortfolio/static/js/runtime-main.09824b92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d550d5d0ebcf4c99090f86d4c4593f85.js"
+  "/SunilPadikarPortfolio/precache-manifest.c17bcf840f447f1340c22513cc2b427a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SunilPadikarPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
